e286e7973b92c3e077b8066dc8134eabc9a15339
xapfeltortexp <jan.hoeck@gmx.de>
2012-11-30 19:49:54 +0100

    Command

--- src/CTF/CTF.java
+++ src/CTF/CTF.java
@@ -1,5 +1,21 @@
 package CTF;
 
-public class CTF {
+import org.bukkit.plugin.java.JavaPlugin;
+
+import CTF.Commands.CTFCommand;
+import CTF.Util.CTFUtil;
+
+public class CTF extends JavaPlugin {
+	
+	public CTFUtil util;
+	public CTFCommand cmd;
+	
+	public void onEnable() {
+		
+		this.util = new CTFUtil();
+		this.cmd = new CTFCommand(this);
+		
+		getCommand("").setExecutor(cmd);
+	}
 
 }
--- src/CTF/Commands/CTFCommand.java
+++ src/CTF/Commands/CTFCommand.java
@@ -1,5 +1,35 @@
 package CTF.Commands;
 
-public class CTFCommand {
+import org.bukkit.command.Command;
+import org.bukkit.command.CommandExecutor;
+import org.bukkit.command.CommandSender;
+import org.bukkit.entity.Player;
+
+import CTF.CTF;
+
+public class CTFCommand implements CommandExecutor {
+	
+	public CTF main;
+	
+	public CTFCommand(CTF main) {
+		this.main = main;
+	}
+	
+	public boolean onCommand(CommandSender sender, Command cmd, String label, String[] args) {
+		
+		if(!(sender instanceof Player)) {
+			return false;
+		}
+		
+		if(cmd.getName().equalsIgnoreCase("ctf")) {
+			
+			if(args.length == 0) {
+				//Help
+			} 
+			
+			
+		}
+		return false;
+	}
 
 }
